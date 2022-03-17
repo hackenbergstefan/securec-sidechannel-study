cf7eca8917deb4f169200344c4a8aca4c73e5e9f
Stefan Hackenberg <mail@stefan-hackenberg.de>
2022-03-17 13:09:29 +0100

    Improve elmo capturing to capture generic assembly code

--- analyze/datasets.py
+++ analyze/datasets.py
@@ -6,8 +6,8 @@ basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 _cache = {}
 
 
-def dataset(name):
-    if name in _cache:
+def dataset(name, force_reload=True):
+    if force_reload is False and name in _cache:
         return _cache[name]
     _cache[name] = np.load(f"{basedir}/data/{name}.npy")
-    return dataset(name)
+    return _cache[name]
